94b16abb9f74dfced7601ebf2f86f702b6f2a194
quantumshiro <hiro.1nakanishi@gmail.com>
2022-03-25 23:46:27 +0900

    change calc method

--- gps_info.py
+++ gps_info.py
@@ -22,9 +22,9 @@ def get_gps(fname):
     # Get latitude and longitude information
     def conv_deg(v):
         # Converting fractions to degrees
-        d = float(v[0])
-        m = float(v[1])
-        s = float(v[2])
+        d = float(v[0][0]) / float(v[0][1])
+        m = float(v[1][0]) / float(v[1][1])
+        s = float(v[2][0]) / float(v[2][1])
         return d + (m / 60.0) + (s / 3600.0)
 
     lat = conv_deg(gps["GPSLatitude"])

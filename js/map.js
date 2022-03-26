db2a4652cd3266e4925f9cef575dacc21ec96f82
Makoto Hamada <makoto.market0113@gmail.com>
2022-03-26 16:11:55 +0900

    fix a little

--- js/map.js
+++ js/map.js
@@ -3,9 +3,10 @@ var map = L.map('mapid', {
   zoom: 17,
 });
 
-var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-});
+var tileLayer = L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+  {attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',}
+);
 
 tileLayer.addTo(map);
 var features = [];
@@ -18,7 +19,7 @@ var place = [{
   // "name": "サントリー美術館",
   "lat": "35.6662186",
   "long": "139.7303961"
-  },]
+},]
 
 // GeoJSON形式で複数個のマーカーを設定する
 for (var i = 0; i < place.length; i++) {
